9e4c95b1422820e01f6c0652b50960aaa61475ab
avinash <avi.kubsad14@gmail.com>
2025-08-17 21:34:37 +0530

    folders

---
+++ Backend/src/index.js
@@ -0,0 +1,11 @@
+//const express = require("express");  //require("express") loads the Express module (a framework for building web applications with Node.js).
+
+import express from "express";
+const app = express();       //  Calling express() creates an Express application instance.
+                             //This app object is what you’ll use to define routes (like app.get("/"...)), 
+                             // middleware, and configuration.
+
+
+app.listen(5001, () => {
+   console.log("app is runing on port 5001");
+})
