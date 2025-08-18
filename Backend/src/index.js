55fd99c6813acb71d4f411cba79f0786de033444
avinash <avi.kubsad14@gmail.com>
2025-08-18 22:12:27 +0530

    auth routes

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,11 +1,15 @@
 //const express = require("express");  //require("express") loads the Express module (a framework for building web applications with Node.js).
 
 import express from "express";
-const app = express();       //  Calling express() creates an Express application instance.
-                             //This app object is what you’ll use to define routes (like app.get("/"...)), 
-                             // middleware, and configuration.
 
+import authRoutes from "./routes/auth.route.js";
+
+const app = express(); //  Calling express() creates an Express application instance.
+//This app object is what you’ll use to define routes (like app.get("/"...)),
+// middleware, and configuration.
+
+app.use("/api/auth", authRoutes);
 
 app.listen(5001, () => {
-   console.log("app is runing on port 5001");
-})
+  console.log("app is runing on port 5001");
+});
