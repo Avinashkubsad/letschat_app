156497f90f3e0483c6794328c37ffc76be4d38c3
Avinash141 <avikubsad14@gmail.com>
2025-09-10 22:46:59 +0530

    new laptop

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -4,7 +4,7 @@ import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
-import cookieParser from "cookies-parser";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 const app = express(); //  Calling express() creates an Express application instance.
