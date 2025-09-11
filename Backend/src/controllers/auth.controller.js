686318bfa774d5df5bbf552f2987df2d6e934c97
Avinash141 <avi.kubsad14@gmail.com>
2025-09-11 23:56:17 +0530

    for update profile check if the token id valid or through cookies

--- Backend/src/controllers/auth.controller.js
+++ Backend/src/controllers/auth.controller.js
@@ -80,9 +80,6 @@ export const login = async(req, res) => {
   }
 };
 
-
-
-
 export const logout = (req, res) => {
   try{
     res.cookie("jwt","",{maxAge:0})   // 0 means it will logout immediately
