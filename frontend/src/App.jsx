a1335556e2d2d00561a62ceedde4c77dd4f500c1
Avinashkubsad <avi.kubsad14@gmail.com>
2025-09-27 18:56:30 +0530

    front end installed tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App = () => (
-  <div className="bg-red-500">
+  <div className="bg-red-500 text-white p-4 text-2xl">
     Hello welcome React World!!!!
   </div>
 )
