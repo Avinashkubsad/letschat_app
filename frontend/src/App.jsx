33917de5af9525c01407d880ff469cfef97c72b3
Avinashkubsad <avi.kubsad14@gmail.com>
2025-09-28 21:04:30 +0530

    fron end cnages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => (
   <div className="bg-red-500 text-white p-4 text-2xl">
-    Hello welcome React World!!!!
+    Hello welcome React World!!!! lets start
   </div>
 )
 
