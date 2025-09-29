0c36ba5ea9d6ba6fc8789edebdbc8c6c5519c082
Avinashkubsad <avi.kubsad14@gmail.com>
2025-09-30 00:10:25 +0530

    pages craeted

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,17 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar.jsx";
 
 import { Routes,Route} from "react-router-dom";
 
 const App = () => (
   <div>
 
-    <Navbar/>
+    <Navbar/>                           //  ← Always visible
+                                   //         ← Only renders the matched route
     <Routes>
       <Route path="/" element={<HomePage />}/>
       <Route path="/signup" element={<SignUpPage />}/>
-      <Route path="/login" element={<SignInpage />}/>
+      <Route path="/login" element={<SignInPage />}/>
       <Route path="/settings" element={<SettingsPage />}/>
       <Route path="/profile" element={<ProfilePage />}/>
 
