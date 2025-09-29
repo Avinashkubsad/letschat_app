264751cc17552ff7a26e60e2fc11ec9cd63fe373
Avinashkubsad <avi.kubsad14@gmail.com>
2025-09-29 23:33:17 +0530

    check daisyui working or not

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const App = () => (
   <div className="bg-red-500 text-white p-4 text-2xl">
-    Hello welcome React World!!!! lets start
+    <button className="btn btn-active">Default</button>
+<button className="btn btn-active btn-primary">Primary</button>
+<button className="btn btn-active btn-secondary">Secondary</button>
+<button className="btn btn-active btn-accent">Accent</button>
+<button className="btn btn-active btn-info">Info</button>
+<button className="btn btn-active btn-success">Success</button>
+<button className="btn btn-active btn-warning">Warning</button>
+<button className="btn btn-active btn-error">Error</button>
   </div>
 )
 
