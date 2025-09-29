6b342af4c70226622e951a94277bfd6b0c151861
Avinashkubsad <avi.kubsad14@gmail.com>
2025-09-30 00:02:17 +0530

    creating pages in pages folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Navbar from "./components/Navbar";
+
+import { Routes,Route} from "react-router-dom";
 
 const App = () => (
   <div>
+
+    <Navbar/>
+    <Routes>
+      <Route path="/" element={<HomePage />}/>
+      <Route path="/signup" element={<SignUpPage />}/>
+      <Route path="/login" element={<SignInpage />}/>
+      <Route path="/settings" element={<SettingsPage />}/>
+      <Route path="/profile" element={<ProfilePage />}/>
+
+
+
+    </Routes>
   </div>
 );
 
