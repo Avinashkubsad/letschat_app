f2211eae9bda6cd85542ab6dff3b2b1076f0a264
Avinashkubsad <avi.kubsad14@gmail.com>
2025-10-02 12:18:22 +0530

    axios file

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,1 +1,8 @@
 //create a instance so that we can use throught our application  
+
+import axios from "axios";
+
+export const axiosInstance = axios.create({
+    baseURL: "http://localhost:5001/api",
+    withCredentials:true,                      //want to send cookies with every request
+})
